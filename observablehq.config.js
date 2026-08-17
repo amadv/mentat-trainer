ba0c3a16c781bf1e2192a05f0f0aa58882acae7c
Aaron Madved <aaronmadved@gmail.com>
2026-08-17 18:52:16 -0400

    update footer

--- observablehq.config.js
+++ observablehq.config.js
@@ -137,7 +137,7 @@ export default {
   // Some additional configuration options and their defaults:
   theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
+  footer: "Built by <a href='https:amadved.com'>amadv<a><br />Maintained with ♥ for the web.", // what to show in the footer (HTML)
   sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
